0ab48267726965a5609d3f1034492a66f42f7b3c
CarelessOutcast <tapiacarlos32@gmail.com>
2024-06-21 16:47:19 -0500

    Add project folder structure

---
+++ src/main/java/com/unemployed/joblessautomationtracker/JoblessautomationtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.unemployed.joblessautomationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblessautomationtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblessautomationtrackerApplication.class, args);
+	}
+
+}
